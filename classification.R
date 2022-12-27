31dc24c4342d180e4ea54a43a57387a442a56317
Fabio Cirelli <fabio.cire.156@gmail.com>
2022-12-27 11:06:08 +0100

    Created the logistic regression model, found optimal threshold, created the confusion matrix

--- classification.R
+++ classification.R
@@ -1,4 +1,6 @@
 library(imbalance)
+library(caret)
+library(pROC)
 
 remove_invalid <- function(data) {
   for (i in 1:8) {
@@ -37,4 +39,29 @@ patients_balanced <- merge(patients, newPDFOS, all = T)
 prevalence <- max(table(patients_balanced$Outcome) / length(patients_balanced[[1]]))
 patients_balanced <- remove_invalid(patients_balanced)
 prevalence <- max(table(patients_balanced$Outcome) / length(patients_balanced[[1]]))
+
+
+patients_balanced$Outcome <- ifelse(patients_balanced$Outcome == 1, 'Unhealthy', 'Healthy')
+patients_balanced$Outcome <- as.factor(patients_balanced$Outcome)
+ctrl <- trainControl(method = 'repeatedcv', number = 10,
+                     repeats = 20, summaryFunction = twoClassSummary,
+                     classProbs = T, savePredictions = T)
+logistic_regression <- train(Outcome ~ ., data = patients_balanced,
+                             method = 'glm', trControl = ctrl, metric = 'ROC')
+
+roc = roc(as.numeric(logistic_regression$trainingData$.outcome=='Unhealthy'),
+          aggregate(Unhealthy~rowIndex,logistic_regression$pred,mean)[,'Unhealthy'], ci=T)
+
+plot(roc, print.auc = T)
+c = coords(roc, x = "best", best.method = 'youden')
+abline(v = c[2])
+abline(h = c[3])
+
+
+cm <- confusionMatrix(predict(logistic_regression, newdata = patients_balanced),
+                      reference = patients_balanced[['Outcome']])
+
+
+
+
 
